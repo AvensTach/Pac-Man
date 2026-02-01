891f90db2f4af665c1cb66606e01abfe5e447a99
Olha <olykagreen220@gmail.com>
2026-02-01 12:31:27 +0200

    add basic game loop and window

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame as pg
+
+pg.init()
+
+SCREEN_WIDTH = 760
+SCREEN_HEIGHT = 760
+
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pg.display.set_caption("Pacman")
+
+clock = pg.time.Clock()
+running = True
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pg.display.flip()
+    clock.tick(60)
+
+pg.quit()
+
