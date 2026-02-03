a0219225673b76864c2d3dac6bddcc2ea20a9339
Artur Tymchuk <artur.tymchuk@gmail.com>
2026-02-03 20:25:40 +0200

    Create Ghost class and basic main file to test

---
+++ ghosts.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Ghost():
+    ''' Class to represent a ghost '''
+    def __init__(self, x:int, y:int, color:tuple, image:str):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.image = image
+        self.frightened = False
+        self.timer = 0
+
+        self.rect = pygame.Rect(self.x, self.y, 30, 30)
+
+    def draw(self, screen):
+        ''' Draw the ghost '''
+        # Change color to blue if they are frightened
+        draw_color = (0, 0, 255) if self.frightened else self.color
+        pygame.draw.rect(screen, draw_color, self.rect)
