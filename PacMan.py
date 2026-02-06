439549644fbb82d6dc5d62baf4686573698c3bdb
Ivan Gozhyi <givan2981@gmail.com>
2026-02-06 12:55:02 +0200

    fixes in class pacman

--- PacMan.py
+++ PacMan.py
@@ -1,17 +1,22 @@
 import pygame as pg
+from settings import TILE_SIZE, Direction
 
 class PacMan:
-    def __init__(self, x, y, tile,speed=2,color=(255, 255, 0)):
-        self.tile = tile
+    def __init__(self, row, col, speed=2):
+        self.x = col * TILE_SIZE
+        self.y = row * TILE_SIZE
+
+
         self.speed = speed
-        self.color=color
 
-        self.pos = pg.Vector2(x * tile, y * tile)
 
-        self.direction = pg.Vector2(0, 0)
-        self.next_direction = pg.Vector2(0, 0)
 
-        self.radius = tile // 2 - 2
+
+        self.direction = Direction.STOP
+        self.next_direction = Direction.STOP
+
+        self.radius = TILE_SIZE// 2 - 2
+        self.color = (255, 255, 0)
 
 
     #INPUT
@@ -19,13 +24,24 @@ class PacMan:
         if event.type != pg.KEYDOWN:
             return
         if event.key in (pg.K_LEFT, pg.K_a):
-            self.next_direction = pg.Vector2(-1, 0)
+            self.next_direction = Direction.LEFT
 
         elif event.key in (pg.K_RIGHT, pg.K_d):
-            self.next_direction = pg.Vector2(1, 0)
+            self.next_direction = Direction.RIGHT
 
         elif event.key in (pg.K_UP, pg.K_w):
-            self.next_direction = pg.Vector2(0, -1)
+            self.next_direction = Direction.UP
 
         elif event.key in (pg.K_DOWN, pg.K_s):
-            self.next_direction = pg.Vector2(0, 1)
+            self.next_direction = Direction.DOWN
+
+    #COLLISION
+    def _can_move(self, layout, direction):
+        dx, dy = direction.value
+
+        new_x, new_y = self.x + dx * self.speed, self.y + dy * self.speed
+
+        col = int((new_x + TILE_SIZE/2) // TILE_SIZE)
+        row = int((new_y + TILE_SIZE / 2) // TILE_SIZE)
+
+        return layout[row][col] == "0"
