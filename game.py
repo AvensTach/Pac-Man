71d0960665ba2c27dc3e2248c6b3ab8a20363871
Artur Tymchuk <artur.tymchuk@gmail.com>
2026-03-09 21:31:10 +0200

    Add class Game and move main.py logic there

---
+++ game.py
@@ -0,0 +1,126 @@
+import pygame as pg
+import settings as s
+import ghosts
+from pacman import Pacman
+from level import Level
+from menu import MainMenuScreen, SettingsScreen
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((s.SCREEN_WIDTH, s.SCREEN_HEIGHT))
+        pg.display.set_caption("Pacman")
+        self.clock = pg.time.Clock()
+        self.running = True
+        self.state = s.STATE_MENU
+
+        # --- Menu Screens ---
+        # Pass instance methods as callbacks
+        self.menu_screen = MainMenuScreen(
+            on_play=self.start_game,
+            on_exit=self.quit_game,
+            on_settings=self.open_settings
+        )
+        self.settings_screen = SettingsScreen(on_back=self.open_menu)
+
+        # Game Entities (Initialized in reset_game)
+        self.level = None
+        self.pacman = None
+        self.ghosts_list = []
+        self.blinky = None
+        self.pinky = None
+        self.inky = None
+        self.clyde = None
+
+    # --- State management callbacks ---
+    def start_game(self):
+        self.reset_game()
+        self.state = s.STATE_PLAYING
+
+    def open_settings(self):
+        self.state = s.STATE_SETTINGS
+
+    def open_menu(self):
+        self.state = s.STATE_MENU
+
+    def quit_game(self):
+        self.running = False
+
+    def reset_game(self):
+        """Initializes a fresh game state."""
+        self.level = Level()
+
+        # Pacman Spawn
+        self.pacman = Pacman(15, 9)
+
+        # Ghost Spawns
+        self.blinky = ghosts.Ghost(7, 9, s.GhostType.BLINKY, self.level, spawn_delay=0)
+        self.pinky = ghosts.Ghost(9, 9, s.GhostType.PINKY, self.level, spawn_delay=2)
+        self.inky = ghosts.Ghost(9, 8, s.GhostType.INKY, self.level, spawn_delay=4)
+        self.clyde = ghosts.Ghost(9, 10, s.GhostType.CLYDE, self.level, spawn_delay=6)
+
+        self.ghosts_list = [self.blinky, self.pinky, self.inky, self.clyde]
+
+    def handle_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+                return
+
+            # Delegate events based on state
+            if self.state == s.STATE_MENU:
+                self.menu_screen.handle_event(event)
+            elif self.state == s.STATE_SETTINGS:
+                self.settings_screen.handle_event(event)
+            elif self.state == s.STATE_PLAYING:
+                if self.pacman:
+                    self.pacman.handle_input(event)
+
+    def update(self):
+        if self.state == s.STATE_PLAYING:
+            # Update ghosts
+            for ghost in self.ghosts_list:
+                ghost.update(self.pacman)
+
+            # Update pacman
+            self.pacman.update(s.LAYOUT, self.level)
+
+            # Interactions
+            self.level.check_pills(self.pacman, self.ghosts_list)
+            self.pacman.check_ghost_collision(self.ghosts_list)
+
+            if not self.pacman.alive:
+                print("Pacman DIED")
+                self.state = s.STATE_MENU
+                self.pacman.alive = True
+
+    def draw(self):
+        if self.state == s.STATE_MENU:
+            self.menu_screen.draw(self.screen)
+
+        elif self.state == s.STATE_SETTINGS:
+            self.settings_screen.draw(self.screen)
+
+        elif self.state == s.STATE_PLAYING:
+            self.screen.fill(s.WALL_COLOR)
+            self.level.draw(self.screen)
+            self.pacman.draw(self.screen)
+
+            for ghost in self.ghosts_list:
+                ghost.draw(self.screen)
+
+            self.level.draw_ui(self.screen)
+
+        pg.display.flip()
+
+    def run(self):
+        """Main Game Loop"""
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.draw()
+            self.clock.tick(s.FPS)
+
+        pg.quit()
+
