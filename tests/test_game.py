a01dfb65325aac6860f1bc04720b5b72ac363dd4
Artur Tymchuk <artur.tymchuk@gmail.com>
2026-04-17 15:12:48 +0300

    Configure ,pylintrc and tests code refactoring and bug fixes

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,26 +1,31 @@
+# pylint: disable=missing-docstring, redefined-outer-name, protected-access, too-many-positional-arguments, too-many-arguments
 import pytest
 import pygame as pg
 from game import Game
 import settings as s
 
+
 @pytest.fixture
 def game_instance():
     # creates a clean game object for each test
-    pg.display.set_mode((1, 1)) # Creating a microwindow for tests
+    pg.display.set_mode((1, 1))  # Creating a microwindow for tests
     return Game()
 
-#Initial State
+
+# Initial State
 def test_game_initial_state(game_instance):
     assert game_instance.state == s.STATE_MENU
     assert game_instance.running is True
 
-#State Transitions
+
+# State Transitions
 def test_game_settings_navigation(game_instance):
     game_instance._open_settings()
     assert game_instance.state == s.STATE_SETTINGS
-    
+
     game_instance._open_menu()
     assert game_instance.state == s.STATE_MENU
+
 
 # Reset Logic
 def test_game_reset_creates_objects(game_instance):
@@ -28,4 +33,4 @@ def test_game_reset_creates_objects(game_instance):
     game_instance._reset_game()
     assert game_instance.pacman is not None
     assert len(game_instance.ghosts_list) == 4
-    assert game_instance.state == s.STATE_MENU # Check that reset does not change the state by itself
+    assert game_instance.state == s.STATE_MENU  # Check that reset does not change the state by itself
